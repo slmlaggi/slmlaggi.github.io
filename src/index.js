d70757a655f0f0c7081da803744d240e41ea8e6f
Linus C <slmlaggiosu@gmail.com>
2024-07-12 21:55:58 +0800

    drafted About page

--- src/index.js
+++ src/index.js
@@ -1,43 +1,138 @@
 /* OVERALL */
 /* ------------------------------------ */
 
-nav_items = document.querySelectorAll(".nav-item");
-nav_btns = document.querySelectorAll(".nav-btn");
+nav_items = document.querySelectorAll('.nav-item');
+nav_btns = document.querySelectorAll('.nav-btn');
 
 for (i=0; i < nav_items.length; i++) {
-    if (nav_btns[i].classList.contains("disabled")) {
-        nav_items[i].style.setProperty("cursor", "not-allowed");
+    if (nav_btns[i].classList.contains('disabled')) {
+        nav_items[i].style.setProperty('cursor', 'not-allowed');
     }
 }
 
+const navbar = document.getElementById('about-nav');
+
+document.addEventListener('scroll', function() {
+    const navbarBottom = navbar.getBoundingClientRect().bottom;
+    const contentElements = document.querySelectorAll('.content'); // Adjust this selector as needed
+
+    let isTouching = false;
+
+    contentElements.forEach(function(element) {
+        const elementTop = element.getBoundingClientRect().top;
+        if (elementTop <= navbarBottom) {
+            isTouching = true;
+        }
+    });
+
+    if (isTouching) {
+        navbar.style.backgroundColor = 'rgb(71, 64, 87)';
+    } else {
+        navbar.style.backgroundColor = 'rgba(25, 25, 25, 0.3)';
+    }
+});
+
+/* ------------------------------------ */
+/* ABOUT */
+/* ------------------------------------ */
+
+document.addEventListener('DOMContentLoaded', function() {
+    var backToTopBtn = document.getElementById('backToTopBtn');
+    var firstFullscreenDiv = document.querySelector('.fullscreen-section');
+
+    // Function to check scroll position and toggle button visibility
+    function toggleBackToTopButton() {
+        if (window.scrollY > firstFullscreenDiv.offsetHeight * .6) {
+            backToTopBtn.style.display = 'block';
+        } else {
+            backToTopBtn.style.display = 'none';
+        }
+    }
+
+    // Add scroll event listener
+    window.addEventListener('scroll', toggleBackToTopButton);
+});
+
+// jQuery function to scroll back to top (accessible globally for onclick attribute)
+$(function() {
+    // Smooth scroll to top
+    $('#backToTopBtn').click(function() {
+        $('html, body').animate({scrollTop: 0}, 'slow');
+        return false;
+    });
+});
+
+$(document).ready(function() {
+    const $scrollIndicator = $('#scroll-indicator');
+    const $sections = $('.fullscreen-section');
+    let currentIndex = 0;
+
+    function scrollToNextSection() {
+        if (currentIndex < $sections.length - 1) {
+            currentIndex++;
+            $('html, body').animate({
+                scrollTop: $sections.eq(currentIndex).offset().top
+            }, 'slow');
+        }
+        updateScrollIndicator();
+    }
+
+    function updateScrollIndicator() {
+        if (currentIndex >= $sections.length - 1) {
+            $scrollIndicator.addClass('hidden');
+        } else {
+            $scrollIndicator.removeClass('hidden');
+        }
+    }
+
+    $scrollIndicator.click(function() {
+        scrollToNextSection();
+        return false;
+    });
+
+    $(window).scroll(function() {
+        const scrollPosition = $(window).scrollTop() + $(window).height();
+        $sections.each(function(index) {
+            if (scrollPosition >= $(this).offset().top + $(this).outerHeight()) {
+                currentIndex = index;
+            }
+        });
+        updateScrollIndicator();
+    });
+
+    // Initial check
+    updateScrollIndicator();
+});
+
+/* ------------------------------------ */
 /* BLOG */
 /* ------------------------------------ */
 
 let posts = [
     {
-        title: "First post!! [09-02-22]",
-        file: "./src/blog_entries/first_post.txt"
+        title: 'First post!! [09-02-22]',
+        file: './src/blog_entries/first_post.txt'
     },
     {
-        title: "Restarting the project~ [15-05-24]",
-        file: "./src/blog_entries/restarting.txt"
+        title: 'Restarting the project~ [15-05-24]',
+        file: './src/blog_entries/restarting.txt'
     },
     // Add more posts here
 ];
 
 function createPost(post) {
-    let postElement = document.createElement("div");
-    postElement.id = post.title.toLowerCase().replace(/\s+/g, "-"); // Making a legible ID for each post
-    postElement.classList.add("post");
+    let postElement = document.createElement('div');
+    postElement.id = post.title.toLowerCase().replace(/\s+/g, '-'); // Making a legible ID for each post
+    postElement.classList.add('post');
 
-    let titleElement = document.createElement("span");
-    titleElement.classList.add("title");
-    titleElement.textContent = "⠀" + post.title; // Adds spacing for the expand/collapse button
+    let titleElement = document.createElement('span');
+    titleElement.classList.add('title');
+    titleElement.textContent = '⠀' + post.title; // Adds spacing for the expand/collapse button
 
-    let contentElement = document.createElement("div");
-    contentElement.classList.add("content");
-    contentElement.style.display = "none"; // Hides the content on load
-    contentElement.innerHTML = ""; // Clear the content
+    let contentElement = document.createElement('div');
+    contentElement.classList.add('content');
+    contentElement.style.display = 'none'; // Hides the content on load
+    contentElement.innerHTML = ''; // Clear the content
 
     // Load post content from file
     fetch(post.file)
@@ -49,23 +144,23 @@ function createPost(post) {
             console.error(`Error loading post content from ${post.file}:`, error);
         });
 
-    let collapseButton = document.createElement("span");
-    let arrow = document.createElement("i");
-    arrow.classList.add("fa-solid");
-    arrow.classList.add("fa-caret-right");
-    arrow.classList.add("fa-2x");
-    collapseButton.classList.add("collapse-button");
+    let collapseButton = document.createElement('span');
+    let arrow = document.createElement('i');
+    arrow.classList.add('fa-solid');
+    arrow.classList.add('fa-caret-right');
+    arrow.classList.add('fa-2x');
+    collapseButton.classList.add('collapse-button');
     collapseButton.appendChild(arrow);
 
-    collapseButton.addEventListener("click", function() {
-        if (contentElement.style.display === "none") {
-            arrow.classList.remove("fa-caret-right");
-            arrow.classList.add("fa-caret-down");
+    collapseButton.addEventListener('click', function() {
+        if (contentElement.style.display === 'none') {
+            arrow.classList.remove('fa-caret-right');
+            arrow.classList.add('fa-caret-down');
         } else {
-            arrow.classList.remove("fa-caret-down");
-            arrow.classList.add("fa-caret-right");
+            arrow.classList.remove('fa-caret-down');
+            arrow.classList.add('fa-caret-right');
         }
-        contentElement.style.display = contentElement.style.display === "none" ? "block" : "none";
+        contentElement.style.display = contentElement.style.display === 'none' ? 'block' : 'none';
     });
 
     postElement.appendChild(collapseButton);
@@ -77,27 +172,27 @@ function createPost(post) {
 }
 
 function loadPage() {
-    let timelineElement = document.querySelector(".timeline");
-    let blogElement = document.getElementById("blog");
-    timelineElement.innerHTML = ""; // Clear the timeline
+    let timelineElement = document.querySelector('.timeline');
+    let blogElement = document.getElementById('blog');
+    timelineElement.innerHTML = ''; // Clear the timeline
 
     for (let i = posts.length - 1; i >= 0; i--) {
         let postElement = createPost(posts[i]);
         blogElement.appendChild(postElement);
 
-        let postTitle = document.createElement("a");
-        postTitle.setAttribute("href", "#" + posts[i].title.toLowerCase().replace(/\s+/g, "-"));
-        postTitle.classList.add("entry");
+        let postTitle = document.createElement('a');
+        postTitle.setAttribute('href', '#' + posts[i].title.toLowerCase().replace(/\s+/g, '-'));
+        postTitle.classList.add('entry');
         postTitle.textContent = posts[i].title;
 
-        postTitle.addEventListener("click", function () {
-            let post = document.getElementById(posts[i].title.toLowerCase().replace(/\s+/g, "-"));
-            if (post.getElementsByTagName("div")[0].style.display === "none") {
-                post.getElementsByTagName("span")[0].click();
+        postTitle.addEventListener('click', function () {
+            let post = document.getElementById(posts[i].title.toLowerCase().replace(/\s+/g, '-'));
+            if (post.getElementsByTagName('div')[0].style.display === 'none') {
+                post.getElementsByTagName('span')[0].click();
             }
 
-            document.querySelector("#blog-nav").style.setProperty("width", "calc(100vw - 210px)");
-            document.querySelector(".post").style.setProperty("width", "calc(100vw - 210px)");
+            document.querySelector('#blog-nav').style.setProperty('width', 'calc(100vw - 210px)');
+            document.querySelector('.post').style.setProperty('width', 'calc(100vw - 210px)');
         })
         timelineElement.appendChild(postTitle);
     }
